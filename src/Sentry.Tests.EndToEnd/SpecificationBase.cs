abf572c07971a5a067c72ddf64101f2915b0183b
Spetz <spetz90@gmail.com>
2016-03-06 08:55:50 +0100

    #4 Created new test project for E2E tests #4.

---
+++ src/Sentry.Tests.EndToEnd/SpecificationBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sentry.Tests.EndToEnd
+{
+    [Specification]
+    public abstract class SpecificationBase
+    {
+        public Exception ExceptionThrown;
+        public bool ExceptionExpected;
+
+        protected virtual async Task EstablishContext()
+        {
+            ExceptionExpected = false;
+        }
+
+        protected virtual async Task BecauseOf()
+        {
+        }
+
+        protected virtual async Task CleanUp()
+        {
+        }
+
+        [OneTimeSetUp]
+        public async Task TestInitialize()
+        {
+            await Task.WhenAll(EstablishContext());
+
+            try
+            {
+                await BecauseOf();
+            }
+            catch (Exception exception)
+            {
+                if (ExceptionExpected == false)
+                    throw;
+
+                ExceptionThrown = exception;
+            }
+        }
+
+        [OneTimeTearDown]
+        public async Task TestCleanup()
+        {
+            await CleanUp();
+        }
+    }
+
+    public class SpecificationAttribute : TestFixtureAttribute
+    {
+    }
+
+    public class ThenAttribute : TestAttribute
+    {
+    }
+}
