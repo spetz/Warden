0f4456c7d16f8892520e7784a3c09b1ad9c10ec4
Spetz <spetz90@gmail.com>
2016-03-07 14:27:58 +0100

    Added example windows service build with TopShelf #7.

--- src/Sentry.Examples.WindowsService/Program.cs
+++ src/Sentry.Examples.WindowsService/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Sentry.Examples.WindowsService
+﻿using Topshelf;
+
+namespace Sentry.Examples.WindowsService
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            HostFactory.Run(x =>
+            {
+                x.Service<SentryService>(s =>
+                {
+                    s.ConstructUsing(name => new SentryService());
+                    s.WhenStarted(async tc => await tc.StartAsync());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription("Sentry");
+                x.SetDisplayName("Sentry");
+                x.SetServiceName("Sentry");
+            });
         }
     }
 }
