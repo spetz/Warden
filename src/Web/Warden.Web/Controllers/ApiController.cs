82cf70361e3a18cf03113f3b29117f0c637454a8
Spetz <spetz90@gmail.com>
2016-04-14 15:43:04 +0200

    Created basic, working web application #47.

---
+++ src/Web/Warden.Web/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Warden.Web.Controllers
+{
+    [Route("api/checks")]
+    public class ApiController : Controller
+    {
+        [HttpPost]
+        public async Task Post([FromBody]Item item)
+        {
+        }
+    }
+
+    //Test data
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
