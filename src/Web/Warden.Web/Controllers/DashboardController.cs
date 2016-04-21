906fe5f8ac762c56badeb2d230f0846193488913
Spetz <spetz90@gmail.com>
2016-04-21 10:50:51 +0200

    Added redirect to the default user's dashboard #48.

--- src/Web/Warden.Web/Controllers/DashboardController.cs
+++ src/Web/Warden.Web/Controllers/DashboardController.cs
@@ -9,17 +9,39 @@ using Warden.Web.ViewModels;
 namespace Warden.Web.Controllers
 {
     [Authorize]
+    [Route("dashboards")]
     public class DashboardController : ControllerBase
     {
         private readonly IOrganizationService _organizationService;
+        private readonly IUserService _userService;
 
-        public DashboardController(IOrganizationService organizationService)
+        public DashboardController(IOrganizationService organizationService,
+            IUserService userService)
         {
             _organizationService = organizationService;
+            _userService = userService;
         }
 
         [HttpGet]
-        [Route("organizations/{id}/dashboard")]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("default")]
+        public async Task<IActionResult> Default()
+        {
+            var user = await _userService.GetAsync(UserId);
+            if (user.RecentlyViewedOrganizationId == Guid.Empty)
+                return RedirectToAction("Index");
+
+            return RedirectToAction("Details", new { id = user.RecentlyViewedOrganizationId });
+        }
+
+        [HttpGet]
+        [Route("organizations/{id}")]
         public async Task<IActionResult> Details(Guid id)
         {
             if (id == Guid.Empty)
