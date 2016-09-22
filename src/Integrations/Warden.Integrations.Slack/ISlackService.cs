f6fa3300a767b4c0f19452e79a58b9dc21bd6ab9
Jan-Pieter Zoutewelle <janpieterzoutewelle@hotmail.com>
2016-09-22 15:11:28 +0200

    Slack integration can also send a colored message.

--- src/Integrations/Warden.Integrations.Slack/ISlackService.cs
+++ src/Integrations/Warden.Integrations.Slack/ISlackService.cs
@@ -22,6 +22,18 @@ namespace Warden.Integrations.Slack
         /// <returns></returns>
         Task SendMessageAsync(string message, string channel = null, string username = null, 
             TimeSpan? timeout = null, bool failFast = false);
+
+        /// <summary>
+        /// Executes the HTTP POST request.
+        /// </summary>
+        /// <param name="message">Message text.</param>
+        /// <param name="valid">Indicates if the message should display a positive or negative color</param>
+        /// <param name="channel">Optional name of channel to which the message will be sent.</param>
+        /// <param name="username">Optional username that will send the message.</param>
+        /// <param name="timeout">Optional timeout for the request.</param>
+        /// <param name="failFast">Optional flag determining whether an exception should be thrown if received reponse is invalid (false by default).</param>
+        Task SendColoredMessageAsync(string message, bool valid, string channel = null, string username = null,
+            TimeSpan? timeout = null, bool failFast = false);
     }
 
     /// <summary>
@@ -49,6 +61,47 @@ namespace Warden.Integrations.Slack
                     channel,
                     username,
                 };
+                var serializedPayload = JsonConvert.SerializeObject(payload);
+                var response = await _httpClient.PostAsync(_webhookUrl, new StringContent(
+                    serializedPayload, Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                    return;
+                if (!failFast)
+                    return;
+
+                throw new IntegrationException("Received invalid HTTP response from Slack API " +
+                                               $"with status code: {response.StatusCode}. Reason phrase: {response.ReasonPhrase}");
+            }
+            catch (Exception exception)
+            {
+                if (!failFast)
+                    return;
+
+                throw new IntegrationException("There was an error while executing the SendMessageAsync(): " +
+                                               $"{exception}", exception);
+            }
+        }
+
+        public async Task SendColoredMessageAsync(string message, bool valid, string channel = null, string username = null,
+            TimeSpan? timeout = null, bool failFast = false)
+        {
+            SetTimeout(timeout);
+            try
+            {
+                var attachment = new
+                {
+                    fallback = message,
+                    color = valid ? "good" : "danger",
+                    fields = new [] { new { value = message } }
+                };
+                var payload = new
+                {
+                    channel,
+                    username,
+                    attachments = new[] {attachment} 
+                };
+
                 var serializedPayload = JsonConvert.SerializeObject(payload);
                 var response = await _httpClient.PostAsync(_webhookUrl, new StringContent(
                     serializedPayload, Encoding.UTF8, "application/json"));
